9a0cfd33adfc6e5bb3b046a9726b88736c8ca546
bkeegan <brenton.keegan@gmail.com>
2013-09-28 01:14:23 -0400

    Changed comments on top for script prereqs from #REQUIRES to #REQUIRE as #REQUIRES is a PS keyword

--- ApplicationMgmt/PSInstallOnce.ps1
+++ ApplicationMgmt/PSInstallOnce.ps1
@@ -20,8 +20,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #>
 
-#REQUIRES: WriteToEventLog (included in this repository)
-#REQUIRES: WritetoLogFile (included in this repository)
+#REQUIRE: WriteToEventLog (included in this repository)
+#REQUIRE: WritetoLogFile (included in this repository)
 #If you do not wish to use these functions you can substitutes the calls to the WriteToEventLog function with the native Powershell variant.
 
 
