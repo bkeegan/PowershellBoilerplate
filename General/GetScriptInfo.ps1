9b86a6338c56a2b1149bd8c3857cc0e3cc6cebf8
bkeegan <brenton.keegan@gmail.com>
2013-12-02 13:41:27 -0500

    Script only collects specified info instead of all info. Updated script with cmdlet binding and best-practice naming.

--- General/GetScriptInfo.ps1
+++ General/GetScriptInfo.ps1
@@ -1,18 +1,51 @@
-function GetScriptInfo($strInfo)
+<#
+Get-ScriptInfo.ps1 - Gets info about the script that is currently running
+
+Copyright (C) 2013  Brenton Keegan
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#>
+function Get-ScriptInfo
 {
-	#quick function to return a selected piece of information about the currently running script.
-	#1. $strInfo - Can be "filename" - will return the name of the .ps1 file, "path" will return the full path of the parent directory and "fullpath" will return the full path of the script, "LOC" (lines of code) will return the amount of lines in a script.
+	<#Description: Gets info about the script that is currently running
+	1. see the switch block below for acceptable input
+	#>
 	
-	$strFilename = Split-Path $MyInvocation.ScriptName -leaf
-	$strPath = Split-Path $MyInvocation.ScriptName
-	$strFullPath = $strPath + "\" + $strFilename
-	[int]$intLOC = Get-Content $strFullPath | Measure-Object -Line | foreach {$_.lines}
 	
-		Switch($strInfo)
-		{
-		"filename"{Return $strFilename}
-		"path"{Return $strPath}
-		"fullpath"{Return $strFullPath}
-		"LOC" {Return $intLOC}
-		}	
+	[cmdletbinding()]
+	Param
+	(
+		[parameter(Mandatory=$true)]
+		[alias("i")] #stands for info
+		[string]$dataToReturn
+	)
+
+	Switch($dataToReturn)
+	{
+		"filename"{$returnData = Split-Path $MyInvocation.ScriptName -leaf}
+		"file"{$returnData = Split-Path $MyInvocation.ScriptName -leaf}
+		
+		"path"{$returnData = Split-Path $MyInvocation.ScriptName}
+		"parentdirectory"{$returnData = Split-Path $MyInvocation.ScriptName}
+		
+		"fullpath"{$returnData = (Split-Path $MyInvocation.ScriptName) + "\" + (Split-Path $MyInvocation.ScriptName -leaf)}
+		
+		"Lines"{$returnData = Get-Content ((Split-Path $MyInvocation.ScriptName) + "\" + (Split-Path $MyInvocation.ScriptName -leaf)) | Measure-Object -Line | foreach {$_.lines}}
+		"LOC" {$returnData = Get-Content ((Split-Path $MyInvocation.ScriptName) + "\" + (Split-Path $MyInvocation.ScriptName -leaf)) | Measure-Object -Line | foreach {$_.lines}}
+	}
+
+	Return $returnData
+	
 }
