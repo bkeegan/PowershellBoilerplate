f2d8c66409344035a78dd77f341cb86ff172356b
bkeegan <brenton.keegan@gmail.com>
2013-12-06 17:39:10 -0500

    Update IncrementIPAddr.ps1

--- General/IncrementIPAddr.ps1
+++ General/IncrementIPAddr.ps1
@@ -1,40 +1,64 @@
-function IncrementIPAddr($strIPAddr)
+<#
+Step-IPv4Address.ps1 - Steps to the next IP address. 
+
+Copyright (C) 2013  Brenton Keegan
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#>
+
+function Step-IPv4Address
 {
-	<#Author: Brenton Keegan - 8/27/13
-	Description: This function takes an inputted IP address and adds one to it.
+	<#Description: Steps to the next IP address. 
 	Example 192.168.0.255 will become 192.168.1.0
 	#>
+		
+	[cmdletbinding()]
+	Param
+	(
+		[parameter(Mandatory=$true,ValueFromPipeline=$true)]
+		[alias("ip")]
+		$ipv4addr
+	)	
 	
 	#split into into array of individually manipulable values
-	$arrOctets = $strIPAddr.split(".")
-
+	$octets = $ipv4addr.split(".")
 
 	#step backwards thru the array until it finds an octet that's not at max (255)
 	for($i=3;$i -ge 0;$i--)
 	{
-		[int]$intOctet = $arrOctets[$i]
-		if([int]$intOctet -lt 255)
+		[int]$octet = $octets[$i]
+		if([int]$octet -lt 255)
 		{
-			$intOctet++
-			$arrOctets[$i] = $intOctet
+			$octet++
+			$octets[$i] = $octet
 			Break
 		}
 		Else
 		{
-			$arrOctets[$i] = 0
+			$octets[$i] = 0
 		}
 	}
 	#reassemble string from array
 	for($i=0;$i -le 3;$i++)
 	{
-		$strOutput = $strOutput + $arrOctets[$i]
+		$output = $output + $octets[$i]
 		If($i -lt 3)
 		{
-			$strOutput = $strOutput + "."
+			$output = $output + "."
 		}
 	}
 	
-	Return $strOutput
-	
+	return $output
 }
-
