8fd799d97f9f31758fe63980e486693d0b628b26
bkeegan <brenton.keegan@gmail.com>
2013-12-06 17:03:37 -0500

    Update MachineObject.ps1

--- General/MachineObject.ps1
+++ General/MachineObject.ps1
@@ -1,174 +1,229 @@
-Function MachineObject($strComputer,$arrDatastore,$bolUSB=$true,$bolLegacy=$true,$hshCreds="",$bolImpersonate=$true)
+<#
+Get-WMIComputer.ps1 - Returns a custom object containing information about a computer gathered via WMI.
+
+Copyright (C) 2013  Brenton Keegan
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#>
+
+#PREREQ: requires ConvertFrom-Bytes - also available in this repository
+
+function Get-WMIComputer
 {
-		<#
-		Description: This function collects information about a machine and stores it in a custom object and stores it in an array of objects. 
-		This function is meant to be be called in a loop thru several targets - be it a subnet or a list of machines from an AD query. 
-		This function will export a CSV with all machines it was able to perform a WMI query on. This script collects fairly detailed information and it handles variable amounts of data being returned from each machine.
-		For example if one machine has 4 ram chips and another 2, the machine with only 2 will have properties added for the other 2 chips with "N/A" as the value.
-		If one machine in a list of targets has 16 ram chips then ALL computers will have properties for 16 indivudal ramchips however chips not present will have values of "N/A"
-		This applies for any set of data with a variable amount of returns (ie, multiple HDs, multiple CPUs, etc)
+        <#
+        Description: Returns a custom object containing information about a computer gathered via WMI. 
+        1. $computer. Target machine. Should enumerate thru list in loop
+        2. $basic - Returns basic info about the machine (hostname, serialnumber, model etc)
+        3. $cpu - returns CPU name(s)
+			4. $extendedCPUInfo - returns clockspeed, data/address width etc)
+			5. $ram - returns total RAM
+			6. $extendedRamInfo - returns info on each ram chip
+			7. $hd - returns HD ID and size
+			8. $extendedHDinfo - returns label and freespace
+			9. $nic - returns nic names and MAC addresses
+			10. $vid - returns video card information
+			11. $optical - returns info on optical disk drives
+			12. $audio - returns info on audio devices.	
+			11. $usb - returns USB bus info
+        #>
+        [cmdletbinding()]
+	Param
+	(
+		[parameter(Mandatory=$true,ValueFromPipeline=$true)]
+		[alias("c")]
+		$computer,
 		
-		#Known Bug: I haven't been able to find a good way to store the properties. So the final CSV might have properties out of order. (ie, it might look something like this CPU,HD, RamChip1,RamChip2,Video,NIC,RamChip3). Data won't be malformed - just not ordered.
-		It's fairly easy to reorder the fields in Excel/LibreOffice after the fact - but me a little bit but it will have to do until I find a way to sort them.
+		[parameter(Mandatory=$false)]
+		[switch]$basic, #returns hostname, model, BIOS version, OS version
 		
-		Parameters:
-		1. $strComputer. Target machine. Should enumerate thru list in loop
-		2. $arrDatastore - array to store all objects. You should declare an array outside of this function and pass it thru this function. This function will add the object that corresponds to the machine and return the array when it's done.
-		3. $bolUSB - report connected USB devices (includes Hubs) - default is true - set this to false to not report on connected usb devices (this can generate a lot of information so I made this one so you can toggle it)
-		4. $bolLegacy (default is $true) - will use DCOM style method of communicating with target machine. set to $false to use PSremoting/WS-MAN instead.
-		5. TODO - $hshCreds - hashtable of credentials to try. By default it will always try to impersonate unless you set $bolImpersonate to $false. Be default this is set to an empty string. If it's not empty it will attempt to enumerate thru the credential pairs 
-		6. TODO - $bolImpersonate - Will not try to impersonate credentials that the script is running as when making WMI connections. If this is set to false than a hashtable must be specified. If set to true it will attempt to impersonate BEFORE iterating thru specified credential list
-		#>
+		[parameter(Mandatory=$false)]
+		[switch]$cpu,
 		
-		#WMI Objects
-		if ($bolLegacy -eq $true)
-		{
-			$objHDs = Get-WMIObject win32_logicaldisk -Filter "DriveType=3" -ComputerName $strComputer
-			$objOS = Get-WMIObject win32_operatingsystem -ComputerName $strComputer
-			$objSystem = Get-WMIObject win32_computersystem -ComputerName $strComputer
-			$objSystemEnclosure = Get-WMIObject win32_systemenclosure -ComputerName $strComputer
-			$objBIOS = Get-WMIObject win32_bios -ComputerName $strComputer
-			$objRAM = Get-WMIObject win32_physicalmemory -ComputerName $strComputer
-			$objRAMArrary = Get-WMIObject win32_physicalmemoryarray -ComputerName $strComputer
-			$objNIC = Get-WMIObject win32_networkadapter -ComputerName $strComputer -filter "Adaptertype='Ethernet 802.3'"
-			$objCPU = Get-WMIObject win32_processor -ComputerName $strComputer
-			$objVideo = Get-WMIObject win32_videocontroller -ComputerName $strComputer
-			$objOptical = Get-WMIObject win32_CDROMDrive -ComputerName $strComputer
-			$objSound = Get-WMIObject win32_sounddevice -ComputerName $strComputer
+		[parameter(Mandatory=$false)]
+		[alias("ecpu")] #returns extended info about the cpus
+		[switch]$extendedCPUInfo,
 		
-		if($bolUSB -eq $true)
-		{
-			$objUSB = Get-WMIObject Win32_USBControllerDevice |%{[wmi]($_.Dependent)}
-		}
+		[parameter(Mandatory=$false)]
+		[switch]$ram,
 		
-		}
-		Else
-		{
-			$objHDs = Get-CIMInstance win32_logicaldisk -Filter "DriveType=3" -ComputerName $strComputer
-			$objOS = Get-CIMInstance win32_operatingsystem -ComputerName $strComputer
-			$objSystem = Get-CIMInstance win32_computersystem -ComputerName $strComputer
-			$objSystemEnclosure = Get-CIMInstance win32_systemenclosure -ComputerName $strComputer
-			$objBIOS = Get-CIMInstance win32_bios -ComputerName $strComputer
-			$objRAM =Get-CIMInstance win32_physicalmemory -ComputerName $strComputer
-			$objRAMArrary = Get-CIMInstance win32_physicalmemoryarray -ComputerName $strComputer
-			$objNIC = Get-CIMInstance win32_networkadapter -ComputerName $strComputer -filter "Adaptertype='Ethernet 802.3'"
-			$objCPU = Get-CIMInstance win32_processor -ComputerName $strComputer
-			$objVideo = Get-CIMInstance win32_videocontroller -ComputerName $strComputer
-			$objOptical = Get-CIMInstance win32_CDROMDrive -ComputerName $strComputer
-			$objSound = Get-CIMInstance win32_sounddevice -ComputerName $strComputer
+		[parameter(Mandatory=$false)]
+		[alias("eram")] #returns ram about each chip.
+		[switch]$extendedRamInfo,
 		
-		if($bolUSB -eq $true)
-		{
-			$objUSB = Get-CIMInstance Win32_USBControllerDevice |%{[wmi]($_.Dependent)}
-		}
+		[parameter(Mandatory=$false)]
+		[switch]$hd,
 		
-		}
+		[parameter(Mandatory=$false)]
+		[alias("ehd")] #returns free/used space.
+		[switch]$extendedHDInfo,
+		
+		[parameter(Mandatory=$false)]
+		[switch]$nic,
 
-		#format RAM measurements
-		
-		#$FormattedRAM 
-		
-		$c = 0 #cpu counter
-		$r = 0 #ram chip counter
-		$n = 0 #NIC counter
-		$h = 0 #HD counters
-		$b = 0 #Video card counter
-		$o = 0 #optical drive counter
-		$u = 0 #usb counter
-		$s = 0 #sound device counter
-		
-		#custom object to store machine info
-		$objSysinfo = New-Object PSObject 
-		
-		#basic info - hostname, model, serial, OS
-		$objSysinfo | Add-Member -Membertype NoteProperty -Name Hostname -Value $objSystem.name
-		$objSysinfo | Add-Member -Membertype NoteProperty -Name Model -Value $objSystem.Model
-		$objSysinfo | Add-Member -Membertype NoteProperty -Name Serial -Value $objSystemEnclosure.SerialNumber
-		$objSysinfo | Add-Member -Membertype NoteProperty -Name OSVersion -Value $objOS.Version
-		
-		#processors
-		Foreach ($procchip in $objCPU)
-		{
-			$objSysinfo | Add-Member -Membertype NoteProperty -Name CPU$($c) -Value $procchip.name
-			$objSysInfo | Add-Member -MemberType NoteProperty -Name CPU$($c)_Clock -value $procchip.MaxClockSpeed
-			$objSysInfo | Add-Member -MemberType NoteProperty -Name CPU$($c)_AddressWidth -value $procchip.AddressWidth
-			$objSysInfo | Add-Member -MemberType NoteProperty -Name CPU$($c)_DataWidth -value $procchip.DataWidth
-			$objSysInfo | Add-Member -MemberType NoteProperty -Name CPU$($c)_L2Cache -value $procchip.L2CacheSize
-			$c++
-			
-		}
-		$FormattedRAM = ConvertBytes($objSystem.TotalPhysicalMemory)
-		$objSysinfo | Add-Member -Membertype NoteProperty -Name Memory -Value $FormattedRAM
-		$objSysinfo | Add-Member -Membertype NoteProperty -Name DIMMSlots -Value $objRAMArrary.MemoryDevices
+		[parameter(Mandatory=$false)]
+		[switch]$vid,
 
-		Foreach($chip in $objRAM)
-		{
-			$FormattedChipRAM = ConvertBytes($chip.Capacity)
-			$objSysinfo | Add-Member -Membertype NoteProperty -Name RAMChip$($r)_Size -Value $FormattedChipRAM
-			$objSysinfo | Add-Member -Membertype NoteProperty -Name RAMChip$($r)_Speed -Value $chip.Speed
-			$r++
+		[parameter(Mandatory=$false)]
+		[switch]$optic,					
+		
+		[parameter(Mandatory=$false)]
+		[switch]$audio,				
 	
-		}
-		
-		Foreach($hd in $objHDs)
+		[parameter(Mandatory=$false)]
+		[switch]$usb
+	)
+
+        #custom object to store machine info
+        $wmiComputer = New-Object PSObject 
+        
+        if($basic -eq $true)
+	{
+		$wmiOS = Get-WMIObject win32_operatingsystem -ComputerName $computer
+		$wmiSystem = Get-WMIObject win32_computersystem -ComputerName $computer
+		$wmiSystemEnclosure = Get-WMIObject win32_systemenclosure -ComputerName $computer
+		$wmiBIOS = Get-WMIObject win32_bios -ComputerName $computer
+		$wmiComputer | Add-Member -Membertype NoteProperty -Name Hostname -Value $wmiSystem.name
+		$wmiComputer | Add-Member -Membertype NoteProperty -Name Model -Value $wmiSystem.Model
+		$wmiComputer | Add-Member -Membertype NoteProperty -Name Serial -Value $wmiSystemEnclosure.SerialNumber
+		$wmiComputer | Add-Member -Membertype NoteProperty -Name OSVersion -Value $wmiOS.Version
+		$wmiComputer | Add-Member -Membertype NoteProperty -Name BIOSVersion -Value $wmiBIOS.SMBIOSBIOSVersion
+        }
+        
+	#CPU
+        if($cpu -eq $true)
+	{
+		$c = 0 #cpu counter
+		$wmiCPU = Get-WMIObject win32_processor -ComputerName $computer
+		foreach ($procChip in $wmiCPU)
 		{
-			$FormattedHDSize = ConvertBytes($hd.size)
-			$FormattedHDFreespace = ConvertBytes($hd.freespace)
-			$objSysinfo | Add-Member -Membertype NoteProperty -Name HD$($h)_ID -Value $hd.DeviceID
-			$objSysinfo | Add-Member -Membertype NoteProperty -Name HD$($h)_Label -Value $hd.VolumeName
-			$objSysinfo | Add-Member -Membertype NoteProperty -Name HD$($h)_Freespace -Value $FormattedHDFreespace
-			$objSysinfo | Add-Member -Membertype NoteProperty -Name HD$($h)_Size -Value $FormattedHDSize
-			$h++
-		
-		}
-		
-		#Nics
-		Foreach ($nic in $objNIC)
-		{
-			$objSysinfo | Add-Member -Membertype NoteProperty -Name NIC$($n) -Value $nic.name
-			$objSysinfo | Add-Member -Membertype NoteProperty -Name NIC$($n)_MACAddress -Value $nic.MACAddress
-			$n++
-		}
-		
-		#video
-		Foreach ($vidcard in $objVideo)
-		{
-			$VidRamFormatted = ConvertBytes($vidcard.AdapterRAM)
-			$objSysinfo | Add-Member -Membertype NoteProperty -Name Video$($v)_Name -Value $vidcard.Name
-			$objSysinfo | Add-Member -Membertype NoteProperty -Name Video$($v)_Memory -Value $VidRamFormatted
-			$v++
-		}
-		
-		#Optical devices
-		Foreach($opticaldrive in $objOptical)
-		{
-			$objSysinfo | Add-Member -MemberType NoteProperty -Name Optical$($o)_ID -Value $opticaldrive.ID
-			$objSysinfo | Add-Member -MemberType NoteProperty -Name Optical$($o)_Name -Value $opticaldrive.Name
-			$o++
-		
-		}
-		
-		#usb devices
-		if($bolUSB -eq $true)
-		{
-			Foreach($usbdevice in $objUSB) 
+			$wmiComputer | Add-Member -Membertype NoteProperty -Name CPU$($c) -Value $procChip.name
+			if($extendedCPUInfo -eq $true)
 			{
-				$objSysinfo | Add-Member -MemberType NoteProperty -Name USBDevice$($u) -Value $usbdevice.Description
-				$u++
-			
+				$wmiComputer | Add-Member -MemberType NoteProperty -Name CPU$($c)_Clock -value $procChip.MaxClockSpeed
+				$wmiComputer | Add-Member -MemberType NoteProperty -Name CPU$($c)_AddressWidth -value $procChip.AddressWidth
+				$wmiComputer | Add-Member -MemberType NoteProperty -Name CPU$($c)_DataWidth -value $procChip.DataWidth
+				$wmiComputer | Add-Member -MemberType NoteProperty -Name CPU$($c)_L2Cache -value $procChip.L2CacheSize
+			}
+			$c++
+		}
+	}
+	#RAM
+	if($ram -eq $true)
+	{
+		$r = 0 #ram chip counter
+		$wmiRAM = Get-WMIObject win32_physicalmemory -ComputerName $computer	
+		$FormattedRAMCapacity = (ConvertFrom-Bytes -b $wmiSystem.TotalPhysicalMemory -bi)
+		$wmiComputer | Add-Member -Membertype NoteProperty -Name Memory -Value $FormattedRAMCapacity
+		if($extendedRamInfo -eq $true)
+		{
+			$wmiRAMArrary = Get-WMIObject win32_physicalmemoryarray -ComputerName $computer		
+			$wmiComputer | Add-Member -Membertype NoteProperty -Name DIMMSlots -Value $wmiRAMArrary.MemoryDevices
+			foreach($chip in $wmiRAM)
+			{
+				$formattedChipCapacity = (ConvertFrom-Bytes -b $chip.Capacity -bi)
+				$wmiComputer | Add-Member -Membertype NoteProperty -Name RAMChip$($r)_Size -Value $formattedChipCapacity
+				$wmiComputer | Add-Member -Membertype NoteProperty -Name RAMChip$($r)_Speed -Value $chip.Speed
+				$r++
 			}
 		}
-		
-		#sound devices
-		Foreach($sounddevice in $objSound)
+	}
+			
+	#HDs
+	if($hd -eq $true)
+	{				
+		$h = 0 #HD counters
+		$wmiHD = Get-WMIObject win32_logicaldisk -Filter "DriveType=3" -ComputerName $computer
+		foreach($hd in $wmiHDs)
 		{
-			$objSysinfo | Add-Member -MemberType NoteProperty -Name Sound$($s)_Name -Value $sounddevice.ProductName
+			$formattedHDSize = (ConvertFrom-Bytes -b $hd.size -bi)
+			$formattedHDFreespace = (ConvertFrom-Bytes -b $hd.freespace bi)
+			$wmiComputer | Add-Member -Membertype NoteProperty -Name HD$($h)_ID -Value $hd.DeviceID
+			$wmiComputer | Add-Member -Membertype NoteProperty -Name HD$($h)_Size -Value $formattedHDSize
+			if($extendedHDInfo -eq $true)
+			{
+				$wmiComputer | Add-Member -Membertype NoteProperty -Name HD$($h)_Label -Value $hd.VolumeName
+				$wmiComputer | Add-Member -Membertype NoteProperty -Name HD$($h)_Freespace -Value $formattedHDFreespace
+			}
+			$h++
+		}
+        }
+        
+        #Nics
+        if($nic -eq $true)
+	{
+		$n = 0 #NIC counter
+		$wmiNIC = Get-WMIObject win32_networkadapter -ComputerName $computer -filter "Adaptertype='Ethernet 802.3'"
+		foreach ($networkCard in $wmiNIC)
+		{
+			$wmiComputer | Add-Member -Membertype NoteProperty -Name NIC$($n) -Value $networkCard.name
+			$wmiComputer | Add-Member -Membertype NoteProperty -Name NIC$($n)_MACAddress -Value $networkCard.MACAddress
+			$n++
+		}
+        }
+        
+        #video
+	if($vid -eq $true)
+	{
+		$v = 0 #Video card counter
+		$wmiVideo = Get-WMIObject win32_videocontroller -ComputerName $computer
+		foreach ($vidcard in $wmiVideo)
+		{
+			$VidRamFormatted = (ConvertFrom-Bytes -b $vidcard.AdapterRAM -bi)
+			$wmiComputer | Add-Member -Membertype NoteProperty -Name Video$($v)_Name -Value $vidcard.Name
+			$wmiComputer | Add-Member -Membertype NoteProperty -Name Video$($v)_Memory -Value $VidRamFormatted
+			$v++
+		}
+        }
+        
+        #Optical devices
+	if($optical -eq $true)
+	{
+		$o = 0 #optical drive counter
+		$wmiOptical = Get-WMIObject win32_CDROMDrive -ComputerName $computer
+		foreach($opticaldrive in $wmiOptical)
+		{
+			$wmiComputer | Add-Member -MemberType NoteProperty -Name Optical$($o)_ID -Value $opticaldrive.ID
+			$wmiComputer | Add-Member -MemberType NoteProperty -Name Optical$($o)_Name -Value $opticaldrive.Name
+			$o++
+		}
+	}
+	
+	#sound devices
+        if($audio -eq $true)
+	{
+		$s = 0 #sound device counter
+		$wmiAudio = Get-WMIObject win32_sounddevice -ComputerName $computer
+		foreach($sounddevice in $wmiSound)
+		{
+			$wmiComputer | Add-Member -MemberType NoteProperty -Name Sound$($s)_Name -Value $sounddevice.ProductName
 			$s++
 		
 		}
-		
-		
-		$objSysinfo | Add-Member -Membertype NoteProperty -Name BIOSVersion -Value $objBIOS.SMBIOSBIOSVersion
-
-		Return $ObjSysInfo
+	}
+        
+        #usb devices
+	if($usb -eq $true)
+        {
+		$u = 0 #usb counter
+		$wmiUSB = Get-WMIObject Win32_USBControllerDevice |%{[wmi]($_.Dependent)} 
+		Foreach($usbdevice in $wmiUSB) 
+		{
+			$wmiComputer | Add-Member -MemberType NoteProperty -Name USBDevice$($u) -Value $usbdevice.Description
+			$u++
+		}
+        }
+        
+        Return $wmiComputer
 }
